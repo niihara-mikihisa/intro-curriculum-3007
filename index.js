468568b82a1f02a08d19fb016ddb28121e403090
Niihara Mikihisa <kennkyuusyawomezasu@gmail.com>
2020-10-20 11:11:55 +0900

    17で割り切れる値を判断するモジュールを作成

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * @param num 整数
+ * @return 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}   
+
 module.exports = {
+    isMultipleOfSeventeen
 }
